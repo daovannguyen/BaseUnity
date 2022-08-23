2ea021890b546a984e874cd1cfe1cc3722add1a4
Đào Văn Nguyên <vannguyen86068000@gmail.com>
2022-08-24 02:14:08 +0700

    2

---
+++ TimeCountDown/2/TimeCountDown.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using UnityEngine.Events;
+
+public class TimeCountDown : MonoBehaviour
+{
+    public float timeDown;
+    //[HideInInspector]
+    public float remainingTime;
+    public bool isCounting;
+    [HideInInspector]
+    public UnityEvent timeUpHandle;
+    [HideInInspector]
+    public UnityEvent updateWhenTimeCount;
+
+    public void SetAgainTimeCount()
+    {
+        enabled = true;
+        isCounting = true;
+        remainingTime = timeDown;
+    }
+    private void Update()
+    {
+        if (isCounting)
+        {
+            remainingTime -= Time.deltaTime;
+            updateWhenTimeCount.Invoke();
+        }
+        if (remainingTime < 0 && isCounting)
+        {
+            enabled = false;
+            timeUpHandle.Invoke();
+            isCounting = false;
+        }
+    }
+    public void FreezeCount()
+    {
+        isCounting = false;
+    }
+    public void UnfreezeCount()
+    {
+        isCounting = true;
+    }
+    public void AddMoreTimeCount(int moreTime)
+    {
+        remainingTime += moreTime;
+    }
+}
---
+++ TimeCountDown/2/UseTimeCountDown.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TimeCountDown))]
+public class UseTimeCountDown : MonoBehaviour
+{
+    TimeCountDown timeCountDown;
+    private void Awake()
+    {
+        timeCountDown = GetComponent<TimeCountDown>();
+        timeCountDown.timeUpHandle.AddListener(SayTimeUp);
+        timeCountDown.updateWhenTimeCount.AddListener(SayRemainingTime);
+        timeCountDown.SetAgainTimeCount();
+    }
+
+    private void SayRemainingTime()
+    {
+        Debug.Log(timeCountDown.remainingTime);
+    }
+
+    private void SayTimeUp()
+    {
+        Debug.Log("Hết giờ rồi bạn ơi");
+    }
+
+}
